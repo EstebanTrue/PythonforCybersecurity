66ae5f6185ec7608a78feb11fa7499c41e8775a0
Britney <stebtrujillo2005@gmail.com>
2023-10-09 09:48:13 -0700

    ping process

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python with Inputs
-# Created 
+# Created Este 10/2
 
 # Suggestion, build out 1 line at a time
 # Once multiple print statemetns exist, put a breakpoint at first print line
 # Then walk through as an example of "debugging"
+
+#ask user for name
+usersname = input("What is your name")
+#say hello to the user
+print("hello mf "+usersname)
