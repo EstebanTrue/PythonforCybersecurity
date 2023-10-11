d110b6a725a9df3d5b52b606ce60edd1f4fcc574
Britney <stebtrujillo2005@gmail.com>
2023-10-11 09:34:26 -0700

    loop and conditionals

--- start/CH03/pinger2.py
+++ start/CH03/pinger2.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
-# Second example of pinging from Python
+# First example of pinging from Python
 # By 
+
+#import modules
+import os
+#build the ping command 
+target = "192.168.0." 
+
+#run ping command
+for x in range(255):
+    count = 1 + x
+    fullA = target + str(count)
+    ping_cmd = "ping -c 1 -w 1 " + fullA + " > /dev/null 2>&1 "
+    exit_code = os.system(ping_cmd)
+    #print results
+    #print(fullA, exit_code)
+
+    if exit_code == 0:
+        print("*****{0} User is Active****" .format(fullA))
+
+    if exit_code != 0:
+        print("{0} User is not online" .format(fullA))
