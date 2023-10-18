e5d981c67e42f14da31c2b5fb9c8704fdbf08fa4
Britney <stebtrujillo2005@gmail.com>
2023-10-18 09:43:52 -0700

    we learned about paths yay

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
 # By 
+import os 
+os_path = os.path.realpath(__file__)
+dir_path = os.path.dirname(os_path)
+#open fule for reading 
+wopper = open(dir_path + "/Testing.txt", "r")
+# read from the file
+file_contents = wopper.read()
+print(file_contents)
+#close file 
+wopper.close()
+# = os.getcwd()
+
+
+print(dir_path)
--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
 # By 
+
+#open and create a file
+wopper = open("Testing.txt", "w")
+#write the file 
+wopper.write("file\n")
+wopper.write("I am Desperado")
+name = input("State thy name?\n")
+wopper.write("WHO ARE YOU TO ANSWER AND OPEN THIS FILE\n")
+piece = input("ANSWER OR FACW THE CONSEQUENSES\n")
+if piece == "im sorry":
+    
+#close the file 
+wopper.close()
