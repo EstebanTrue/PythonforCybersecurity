96f29c98bad0cd1107971f81119fc39f9e8b5e8b
Britney <stebtrujillo2005@gmail.com>
2023-11-08 09:49:07 -0800

    UM watch the recordninf

--- start/CH07/Find404.py
+++ start/CH07/Find404.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python3
 # Script that scans web server logs for 404 errors
 # By 
+
+import os
+import re
+
+os_path = os.path.realpath(__file__)
+dir_path = os.path.dirname(os_path)
+logFile = open(dir_path + "/access.log", "r")
+
+ipDict = {}
+
+
+statusDict = {}
+while True:
+    line = logFile.readline()
+    if not line:
+        break
+
+    # check for 404
+    if "404" in line:
+        lineParts = line.split()
+        ipAddr = lineParts[0]
+        if ipAddr not in ipDict.keys():
+            ipDict[ipAddr] = 1
+        else:
+            ipDict[ipAddr] += 1
+logFile.close()
+for w in sorted(ipDict, key = ipDict.get, reverse = False):
+    print(w, ipDict[w])
--- start/CH07/FindStatusCodes.py
+++ start/CH07/FindStatusCodes.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
-# Script that scans web server logs for status codes
-# Use RegEx to find and report on most frequent status messages
+# Script that scans web server logs for 404 errors
 # By 
+
+import os
+import re
+
+os_path = os.path.realpath(__file__)
+dir_path = os.path.dirname(os_path)
+logFile = open(dir_path + "/access.log", "r")
+
+ipDict = {}
+#setup regeex pattern
+statusPattern = r'\s(\d{3})\s'
+statusDict = {}
+while True:
+    line = logFile.readline()
+    if not line:
+        break
+    m = re.search(statusPattern, line)
+    if m:
+        code = m.group()
+        if code in statusDict.keys():
+            statusDict[code] += 1
+        else:
+            statusDict[code] += 1
+   
+logFile.close()
+for w in sorted(statusDict, key = statusDict.get, reverse = False):
+    print(w, statusDict[w])
