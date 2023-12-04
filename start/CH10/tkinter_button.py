df05b5770247bbac8ed93b5b6b88b11ffe744e4d
Britney <stebtrujillo2005@gmail.com>
2023-12-04 09:53:40 -0800

    gui information  yayyy

--- start/CH10/tkinter_button.py
+++ start/CH10/tkinter_button.py
@@ -3,11 +3,32 @@
 # Create 
 
 # Import tkinter
-
+import tkinter
+from tkinter import messagebox
 # Functions
-
+def buttonHelloClick():
+    tkinter.Label(windowMain, text="hi :)").grid(column=1, row=1)
+    message = entryMessage.get()
+    labelHello.config(text=":)")
+    messagebox.showerror("Nuh-uh-uhhh", message)
 # Create the GUI main window
-
+windowMain = tkinter.Tk()
 # Add widgets
+labelHello = tkinter.Label(windowMain, \
+    text= "Hello there", \
+    font=("Arial Bold", 90))
+    #you cant use both ppack and grid in the same window because it wont work
+#labelHello.pack()
+labelHello.grid(column=0, row=0)
+#button
+buttonHello = tkinter.Button(windowMain, \
+    text= "click here", \
+    command= buttonHelloClick) #.pack()
+buttonHello.grid(column=1, row=0)
+#text box
+entryMessage = tkinter.Entry(windowMain, width=50)
+#the grid must be seperate or the computer will be wierd about itand not work 
+entryMessage.grid(column=0, row=2)
 
 # Enter the main event loop
+windowMain.mainloop()
--- start/CH10/tkinter_hello.py
+++ start/CH10/tkinter_hello.py
@@ -2,9 +2,15 @@
 # Create by 
 
 # Import tkinter
-
+import tkinter
 # Create the GUI main window
-
+windowMain = tkinter.Tk()
 # Add widgets
+labelHello = tkinter.Label(windowMain, \
+    text="hwllo Wwrld", \
+    font= ("D050000L", 180) \
+    bg= 'red' )
+labelHello.pack()
 
 # Enter the main event loop
+windowMain.mainloop()
